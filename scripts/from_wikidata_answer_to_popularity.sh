73e04357b5a2ff54212916a24b046aa8d3ce6cfc
BaptBeuns <benetb@eleves.enpc.fr>
2016-06-08 17:34:05 +0200

    Moved every script to script folder

--- scripts/from_wikidata_answer_to_popularity.sh
+++ scripts/from_wikidata_answer_to_popularity.sh
@@ -5,7 +5,7 @@ do
     # Gets the name of the celebrity
     URL=$(echo $LINE | awk -F "\"*,\"*" '{print $1}')
     PERSON=$(echo $LINE | awk -F "\"*,\"*" '{print $2}')
-    SCORE=$(./get_google_score.py "$PERSON")
+    SCORE=$(./scripts/get_google_score.py "$PERSON")
     echo "${SCORE},${URL},${PERSON}";
 done < ${INPUT_NAME}
 # done < <(head -10 footballers.csv)
