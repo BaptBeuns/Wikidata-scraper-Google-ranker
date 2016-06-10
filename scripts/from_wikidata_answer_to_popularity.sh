e58ed15f23798c3c72eb99473c1f168aa6be2aa9
BaptBeuns <benetb@eleves.enpc.fr>
2016-06-10 18:18:54 +0200

    Bugfix: ALWAYS DO LIKE THIS
    On affiche en premier le nom du mec dans SPARQL pour pas qu'on se retrouve avec un caractère en fin de ligne qui fait tout planter.

--- scripts/from_wikidata_answer_to_popularity.sh
+++ scripts/from_wikidata_answer_to_popularity.sh
@@ -3,9 +3,10 @@ INPUT_NAME=$1
 while read LINE;
 do
     # Gets the name of the celebrity
-    URL=$(echo $LINE | awk -F "\"*,\"*" '{print $1}')
-    PERSON=$(echo $LINE | awk -F "\"*,\"*" '{print $2}')
+    URL=$(echo $LINE | awk -F "\"*,\"*" '{print $2}')
+    PERSON=$(echo $LINE | awk -F "\"*,\"*" '{print $1}')
     SCORE=$(./scripts/get_google_score.py "$PERSON")
-    echo "${SCORE},${URL},${PERSON}";
+    # echo ${PERSON}
+    echo ${PERSON}", "${SCORE}", "${URL};
 done < ${INPUT_NAME}
 # done < <(head -10 footballers.csv)
