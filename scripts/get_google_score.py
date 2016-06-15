5a5f6486813ff484a501e5a1c7dc844e5720d274
BaptBeuns <benetb@eleves.enpc.fr>
2016-06-15 17:39:46 +0200

    Takes as input a file instead of a name

--- scripts/get_google_score.py
+++ scripts/get_google_score.py
@@ -1,26 +1,43 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 """
 Example of Python client calling Knowledge Graph Search API.
-Call by ./google.py 'ARG1' and it will give you the more relevant Person.
 Can be modified for other purposes -> retrieve photo of the person, wikipedia description...
 """
 import json
 import urllib
 import sys
+import io
 
-api_key = open('.api_key').read()
-query = 'Taylor Swift' if len(sys.argv)==1 else sys.argv[1]
-service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
-params = {
-    'query': query,
-    'limit': 1,
-    'indent': True,
-    'key': api_key,
-    'types': 'Person',
-}
-url = service_url + '?' + urllib.urlencode(params)
-response = json.loads(urllib.urlopen(url).read())
-if len(response['itemListElement']) > 0:
-  element = response['itemListElement'][0]
-  #print element['result']['name'] + ',' + str(element['resultScore'])
-  print '%07d, <a href="%s">%s</a>' % (int(element['resultScore']), element['result']['detailedDescription']['url'], element['result']['detailedDescription']['url'])
+file_name = u"" if len(sys.argv)==1 else sys.argv[1]
+
+with io.open(file_name,'r',encoding='utf8') as fileId:
+    f = fileId.read()
+
+names = f.split('\n')
+
+for query in names:
+    if query != u'':
+        api_key = open('.api_key').read()
+        service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+
+        # Necessary to avoid the unicode errors
+        query_name = query.split(',')[0]
+        if isinstance(query_name, unicode):
+            query_name = query_name.encode('UTF-8')
+
+        params = {
+            'query': query_name,
+            'limit': 1,
+            'indent': True,
+            'key': api_key,
+            'types': 'Person',
+        }
+        url = service_url + '?' + urllib.urlencode(params)
+        response = json.loads(urllib.urlopen(url).read())
+        if len(response['itemListElement']) > 0:
+            element = response['itemListElement'][0]
+            score = str(int(element['resultScore']))
+            name = element['result']['name']
+            url = element['result']['detailedDescription']['url']
+            print(u"%s, %s, <a href='%s'>%s</a>" % (query.split(',')[0], score, url, name))
